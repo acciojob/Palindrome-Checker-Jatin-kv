2f2630c6adba86bba9561eea51428ffa31a8842b
Jatin-kv <120176047+Jatin-kv@users.noreply.github.com>
2023-01-30 12:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-// complete the given function
-
 function palindrome(str){
-
+  str=str.toLowerCase();
+   let arr=Array.from(str);
+	let i=0;
+	let j=arr.length-1;
+	while(i<=j){
+		if(arr[i]!=arr[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 }
 module.exports = palindrome
+
+
+
